dd362fa0abf337e51039a5789f027a3a47cd2002
Joachim Penk <jp@figo.io>
2015-12-28 20:40:05 +0100

    added new fingerprint for api.figo.me

--- figo/Config.php
+++ figo/Config.php
@@ -33,7 +33,7 @@ class Config {
     public static $API_ENDPOINT = "api.figo.me";
 
     /** @var string figo Connect SSL/TLS certificate fingerprints */
-    public static $VALID_FINGERPRINTS = array("3A:62:54:4D:86:B4:34:38:EA:34:64:4E:95:10:A9:FF:37:27:69:C0",
+    public static $VALID_FINGERPRINTS = array("38:AE:4A:32:6F:16:EA:15:81:33:8B:B0:D8:E4:A6:35:E7:27:F1:07",
                                               "CF:C1:BC:7F:6A:16:09:2B:10:83:8A:B0:22:4F:3A:65:D2:70:D7:3E");
 }
 
