e922f960fa27c1978d5f3630be48ea98f3962c73
Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
2017-02-28 10:16:16 +0100

    remove empty fingerprint array item because we don't need two anymore

--- figo/Config.php
+++ figo/Config.php
@@ -33,8 +33,7 @@ class Config {
     public static $API_ENDPOINT = "api.figo.me";
 
     /** @var string figo Connect SSL/TLS certificate fingerprints */
-    public static $VALID_FINGERPRINTS = array("",
-                                              "07:0F:14:AE:B9:4A:FB:3D:F8:00:E8:2B:69:A8:51:5C:EE:D2:F5:B1:BA:89:7B:EF:64:32:45:8F:61:CF:9E:33");
+    public static $VALID_FINGERPRINTS = array("07:0F:14:AE:B9:4A:FB:3D:F8:00:E8:2B:69:A8:51:5C:EE:D2:F5:B1:BA:89:7B:EF:64:32:45:8F:61:CF:9E:33");
     /**
      * @var string User agent used for API requests
      */
