7a700b92f9434f5dfa74669e6c65b3f4717ff7c1
achimsen <jp@figo.io>
2017-06-02 12:06:26 +0200

    Update Config.php

--- figo/Config.php
+++ figo/Config.php
@@ -33,7 +33,8 @@ class Config {
     public static $API_ENDPOINT = "api.figo.me";
 
     /** @var string figo Connect SSL/TLS certificate fingerprints */
-    public static $VALID_FINGERPRINTS = array("07:0F:14:AE:B9:4A:FB:3D:F8:00:E8:2B:69:A8:51:5C:EE:D2:F5:B1:BA:89:7B:EF:64:32:45:8F:61:CF:9E:33");
+    public static $VALID_FINGERPRINTS = array("07:0F:14:AE:B9:4A:FB:3D:F8:00:E8:2B:69:A8:51:5C:EE:D2:F5:B1:BA:89:7B:EF:64:32:45:8F:61:CF:9E:33",
+                                              "79:B2:A2:93:00:85:3B:06:92:B1:B5:F2:24:79:48:58:3A:A5:22:0F:C5:CD:E9:49:9A:C8:45:1E:DB:E0:DA:50");
     /**
      * @var string User agent used for API requests
      */
@@ -41,7 +42,7 @@ class Config {
     /**
      * @var string Version of this SDK, used in user agent for API requests
      */
-    public static $SDK_VERSION = 'v1.1.4';
+    public static $SDK_VERSION = 'v1.1.5';
 }
 
 ?>
