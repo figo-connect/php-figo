ef7896b8ab31752f1feb91636f49d3fa67f2d10a
Jan van Esdonk <jve@figo.me>
2017-11-21 13:29:12 +0100

    fix endpoint parsing

--- figo/utils.php
+++ figo/utils.php
@@ -2,11 +2,12 @@
 
 function parse_api_endpoint($api_endpoint)   {
     $api_endpoint = rtrim($api_endpoint, "/");
-    $length = strlen($api_endpoint);
-    if (substr($api_endpoint, 0, $length) != "https://")    {
+    if (substr($api_endpoint, 0, 8) != "https://")    {
         $api_endpoint = "https://" . $api_endpoint;
     }
     $api_url = parse_url($api_endpoint);
-    $api_url["path"] = "";
+    if (array_key_exists("path", $api_url) == false)    {
+        $api_url["path"] = "";
+    }
     return $api_url;
 }
